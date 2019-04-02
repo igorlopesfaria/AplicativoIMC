46c4e7ae2b21e926bbf9f411d169ce613a171a3f
Igor Faria <igor_faria@mac-02.local>
2019-04-02 15:25:52 -0300

    Initial Commit

---
+++ AplicativoIMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AplicativoIMC
+//
+//  Created by Igor Faria on 02/04/19.
+//  Copyright © 2019 Igor Faria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
